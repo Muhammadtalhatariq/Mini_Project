c7fe8be098661449432424bc2ef6748dd865acab
muhammadtalha <mtalhatariqx@gmail.com>
2024-07-11 01:09:43 -0700

    update

--- app.js
+++ app.js
@@ -15,30 +15,42 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/login", isLoogedIn, (req, res) => {
+app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("/profile", (req, res) => {
-    console.log(req.user);
-  res.render("login");
+app.get("/profile", isLoogedIn, async(req, res) => {
+let user = await userModel.findOne({email :req.user.email}).populate("posts")
+  res.render("profile", {user});
 });
+app.post("/post", isLoogedIn, async(req, res) => {
+  let user = await userModel.findOne({email :req.user.email})
+   let { content } = req.body
+
+let post = await postModel.create({
+  user : user._id,
+  content
+})
+user.posts.push(post._id)
+await user.save()
+res.redirect("/profile")
+  });
 
 app.get("/logout", (req, res) => {
   res.cookie("token", "");
-  res.redirect("/login");
+ res.redirect("/login")
 });
 app.post("/login", async (req, res) => {
   let { email, password } = req.body;
 
-  let user = await userModel.findOne({ email });
+  let user = await userModel.findOne({email});
   if (!user) return res.status(500).json("something went wrong");
 
   bcrypt.compare(password, user.password, function (err, result) {
     if (result) {
       let token = jwt.sign({ email: email, userid: user._id }, "talha");
       res.cookie("token", token);
-      res.status(500).send("you can login");
+      res.status(500).redirect("/profile")
     } 
     else res.redirect("/login");
   });
@@ -67,10 +79,18 @@ app.post("/register", async (req, res) => {
   });
 });
 
-function isLoogedIn(res, req, next){
- console.log(req.cookies);
- next()
+
+
+
+function isLoogedIn(req, res, next){
+if(req.cookies.token == "") res.redirect("/login")
+  else{
+let data = jwt.verify(req.cookies.token , "talha");
+req.user = data;
+next()
   }
+
+}
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
